9e8397588d45392412b1d7d4c80b2d70c0e92f70
Gaurav Shukla <gaurav@gauravshukla.xyz>
2016-12-26 17:24:19 +0530

    partially completed

--- scripts/controllers/projects.js
+++ scripts/controllers/projects.js
@@ -4,7 +4,7 @@ angular.module('deathping1994githubioApp')
 
     $scope.contributions=[
                         {
-                          name:"Apache Concerted",
+                          name:"Kaivalya Communications",
                           url:"https://github.com/apache/incubator-concerted",
                           description:"Concerted is a next generation big data engine aimed at supporting massive in memory reads for OLAP support.The engine is flexible, with no single point of entry and flexibility to use APIs suited for each use case with full scalability.Concerted allows applications to use the native APIs to have the flexibility to store, access, scale data in memory on demand. No auxillary infrastructure is needed.Concerted implements its own locking manager and transaction manager utilizing the object oriented model,thus greatly simplifying the management of processes like commit and abort, depending a lot on the OS's native support.",
                         },
@@ -38,22 +38,5 @@ angular.module('deathping1994githubioApp')
         name: "Comedy Video | Shit People Do on Facebook",
         src: "https://www.youtube.com/embed/m4fRp8zeuKY",
         description: "Acted and Scripted a comedy video in collaboration with youtube channel -  `The Placebo Fusion`",
-      },
-      {
-        name: "Parallax",
-        url: "https://github.com/Bhramastra/parallax",
-        code: "https://github.com/Bhramastra/parallax",
-        img: "images/parallax.png",
-        description: "Python Framework for asynchronous and distributed programming (Buggy and incomplete)",
-        stack: "Python",
-        presentation: "https://docs.google.com/presentation/d/1KruvuHJXD6IgVdbRB0JnlMNmK5dh6lMlHbPOlapwLhY/edit?usp=sharing"
-      },
-      {
-        name: "Cyberroam Brute",
-        url: "https://github.com/deathping1994/Cyberoam-Brute",
-        code: "https://github.com/deathping1994/Cyberoam-Brute",
-        img: "images/cyberroam.png",
-        description: "A python script to carry out brute force attack on JIIT cyberoam portal.(By Far the most used script I ever wrote)",
-        stack: "Python"
       }];
   });
